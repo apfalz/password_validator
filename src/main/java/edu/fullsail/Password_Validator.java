c8eea05295309a502eaf39b83e2e496ec90a32aa
Andrew Pfalz <apfalz@cct.lsu.edu>
2018-07-03 11:15:32 -0500

    add comment at top of class

--- src/main/java/edu/fullsail/Password_Validator.java
+++ src/main/java/edu/fullsail/Password_Validator.java
@@ -11,6 +11,19 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.IOException;
 
+/*
+*author : Andrew Pfalz
+*  date : 7/2/18
+*  usage:
+*     To build project from project root directory run:
+*         `mvn package`
+*
+*     To execute built package from `target/classes/` run:
+*         `java password_validator.Password_Validator`
+*/
+
+
+
 public class Password_Validator{
 
 
